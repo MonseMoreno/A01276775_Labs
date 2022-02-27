915bc9b03f57c5a04c79b90cc15b22f60b9de8dc
Monse Casas <a01276775@tec.mx>
2022-02-27 16:17:05 -0600

    Se agregaron respuestas a las preguntas y las descripciones de los productos

--- Lab6/java_lab6.js
+++ Lab6/java_lab6.js
@@ -22,7 +22,7 @@ var iva = .07;
 function Mostrar_pregunta(){
      let div_pregunta = document.getElementById ("Pregunta");
      let pregunta = "<h5>¿Por qué es una buena práctica usar JavaScript para checar que sean válidos los inputs de las formas antes de enviar los datos al servidor? </h5> <br>";
-     let respuesta = "Lorem ipsum dolor sit amet consectetur adipiscing, elit ante viverra nostra ornare. Placerat nisl bibendum sociosqu pulvinar euismod velit neque mattis nunc, luctus dui mus tristique nibh nulla nisi eget interdum nec, tempor class suspendisse dis maecenas quis rhoncus libero. Dui ultricies id aliquam elementum venenatis taciti sed tempus placerat sem, per integer lectus fusce vitae nascetur pharetra risus odio, curabitur natoque eu vestibulum interdum montes varius massa urna. Velit vel inceptos a luctus hendrerit rhoncus gravida ullamcorper libero, nibh malesuada suspendisse et penatibus aliquam tempor.";
+     let respuesta = "Porque en caso de que se requiera realizar un input con los datos insertados por usuario y este cometa algun error se le pueda comunicar de manera inmediata en vez de esperar alguna respuesta del servidor";
 
     div_pregunta.innerHTML =  pregunta + respuesta;
     document.getElementById("Pregunta1").onclick=  Vaciar_pregunta;
@@ -31,7 +31,7 @@ function Mostrar_pregunta(){
 document.getElementById("Pregunta2").onclick = () => {
     let div_pregunta = document.getElementById ("Pregunta");
     let pregunta = "<h5>¿Cómo puedes saltarte la seguridad de validaciones hechas con JavaScript? </h5> <br>";
-    let respuesta = "Lorem ipsum dolor sit amet consectetur adipiscing, elit ante viverra nostra ornare. Placerat nisl bibendum sociosqu pulvinar euismod velit neque mattis nunc, luctus dui mus tristique nibh nulla nisi eget interdum nec, tempor class suspendisse dis maecenas quis rhoncus libero. Dui ultricies id aliquam elementum venenatis taciti sed tempus placerat sem, per integer lectus fusce vitae nascetur pharetra risus odio, curabitur natoque eu vestibulum interdum montes varius massa urna. Velit vel inceptos a luctus hendrerit rhoncus gravida ullamcorper libero, nibh malesuada suspendisse et penatibus aliquam tempor.";
+    let respuesta = "Simplemente introduciendo un par de palabras reservadas en el código HTML utilizando la consola del desarrollador deL navegador. Igualmente basta con deshabilitar el javascript en el navegador o hacer directamente el form.submit";
 
    div_pregunta.innerHTML =  pregunta + respuesta;
 
@@ -40,7 +40,7 @@ document.getElementById("Pregunta2").onclick = () => {
 document.getElementById("Pregunta3").onclick = () => {
     let div_pregunta = document.getElementById ("Pregunta");
     let pregunta = "<h5>Si te puedes saltar la seguridad de las validaciones de JavaScript, entonces ¿por qué la primera pregunta dice que es una buena práctica?</h5> <br>";
-    let respuesta = "Lorem ipsum dolor sit amet consectetur adipiscing, elit ante viverra nostra ornare. Placerat nisl bibendum sociosqu pulvinar euismod velit neque mattis nunc, luctus dui mus tristique nibh nulla nisi eget interdum nec, tempor class suspendisse dis maecenas quis rhoncus libero. Dui ultricies id aliquam elementum venenatis taciti sed tempus placerat sem, per integer lectus fusce vitae nascetur pharetra risus odio, curabitur natoque eu vestibulum interdum montes varius massa urna. Velit vel inceptos a luctus hendrerit rhoncus gravida ullamcorper libero, nibh malesuada suspendisse et penatibus aliquam tempor.";
+    let respuesta = "Es buena practica porque como ya se menciono tiene la utilidad de notificarle al usuario si existe algun error en los datos de manera inmediata. Pero también es buena práctica utilizar algoritmos de encriptacion para evitar que cualquier usuario tenga acceso a la informacion  de las variables.";
     div_pregunta.innerHTML =  pregunta + respuesta;
 }
 
@@ -53,7 +53,7 @@ function verificar_contraseña(){
     const ver_contra =  document.getElementById("Verificar_contraseña").value;
 
     if (contra == ver_contra && contra != "" &&  User != ""){
-        div_saludo.innerHTML = '< <div class="col s9 m3"> <div class="card-panel teal lighten-4"> <span> Hola! ' +  User  + '<br> Contraseña correctamente verificada!!</span></div></div>'
+        div_saludo.innerHTML = '<div class="col s9 m3"> <div class="card-panel teal lighten-4"> <span> Hola! ' +  User  + '<br> Contraseña correctamente verificada!!</span></div></div>'
     }
 
     else if (contra != ver_contra && contra != ""  &&  User != ""){
@@ -83,6 +83,7 @@ document.getElementById("cantidad_mochila_3").onchange = () => {
 }
 
 function Calcular_precio(){
+    
     console.log("cantidad mochila 1: " + cantidad_mochila1);
     console.log("cantidad mochila 2: " + cantidad_mochila2);
     console.log("cantidad mochila 3: " + cantidad_mochila3);
@@ -93,7 +94,8 @@ function Calcular_precio(){
     console.log(precio_total);
 
     let div_pregunta = document.getElementById ("precio");
-    div_pregunta.innerHTML =  "<h4> El total de su compra es: $" + precio_total + " MXN</h4>" ;
+    div_pregunta.innerHTML =  "<h4> El total de su compra es: $" + precio_total + " MXN</h4> <br>" ;
+    Mostrar_compra();
 
 }
 
@@ -101,7 +103,13 @@ function Calcular_precio(){
 
 
 
+function Mostrar_compra(){
 
+    let div_compra = document.getElementById ("compra");
+    div_compra.innerHTML = "<h5> " + cantidad_mochila1 + " x" + " Mochila negra con blanco  </h5> <br>" + "<h5> " + cantidad_mochila2 + " x" + " Mochila negra con rosa </h5>  <br>" + "<h5> " + cantidad_mochila3 + " x" + " Mochila negra con amarillo </h5>  <br>" ;
+
+    
+}
 
 
 
