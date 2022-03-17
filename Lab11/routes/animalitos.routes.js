ded799a4877e7c378ce8740014bcca5f4393c7c8
Monse Casas <a01276775@tec.mx>
2022-03-17 13:22:25 -0600

    Correccion animales

--- Lab11/routes/animalitos.routes.js
+++ Lab11/routes/animalitos.routes.js
@@ -41,7 +41,7 @@ router.use('/', (request, response, next) => {
     let respuesta = '<!DOCTYPE html> <html lang="es"> <head> <meta charset="utf-8" /> <meta name="viewport" content="width=device-width, initial-scale=1"> <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css"> <title> A01276775_Lab10 </title> </head> <body> <header> <nav id = "navbar"> <div class="nav-wrapper ">  <img src="https://cdn-icons-png.flaticon.com/512/2325/2325325.png" alt="" width="65" height="60" class="d-inline-block align-text-top" > <ul id="nav-mobile" class="right hide-on-med-and-down"> <li> &nbsp Monserrat Karime Moreno Casas A01276775 &nbsp </li></ul></div></nav></header>';
     respuesta += '<div class="Contenido "> <h1><FONT COLOR="blue"><center> Un animalito para ti! </center> </h1></FONT></h1></div>';
     //for (let i in comida) {
-    respuesta +='  <img  src = ' + animalitos[Math.floor(Math.random() * 4) + 1] + ' width = 400px height = 250px; alt = "Imagen Animalito"/> ';
+    respuesta +='  <div align="center"> <img  src = ' + animalitos[Math.floor(Math.random() * 4) + 1] + ' width = 400px height = 250px; alt = "Imagen Animalito"/> </div>';
    // } 
     
     response.send(respuesta); 
